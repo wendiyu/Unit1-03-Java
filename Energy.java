e2c7341aa0bd266950220f565e881bfb7a9e5900
wendiyu <36240636+wendiyu@users.noreply.github.com>
2019-02-05 18:30:44 -0500

    Create Energy.java

---
+++ Energy.java
@@ -0,0 +1,31 @@
+/****************************************************************************
+ *
+ * Created by: Wendi Yu
+ * Created on: Feb 05 2019
+ * Created for: ICS4U
+ * This program calculates whatever mass (kg) the user inputs to energy (J)
+ *
+ ****************************************************************************/
+import java.util.Scanner;
+
+public class Energy {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// Constant
+		
+		double LIGHTSPEED = 299800000;
+		
+		System.out.println("Enter the mass of an object in kg : ");
+		
+		double mass = scanner.nextDouble();
+		scanner.close();
+		
+		double energy = mass * LIGHTSPEED;
+		
+		System.out.println("If " + mass + " kg of energy was converted to Joule (J). It will produce "+ energy + "J of energy.");
+
+	}
+
+}
